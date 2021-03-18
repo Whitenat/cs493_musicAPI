f13c9bc5b27c9173442b6bfdfa357155491ed0a3
Natashia White <whitenat@oregonstate.edu>
2021-03-18 09:12:15 -0700

    add auth and play endpoint

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const express = require('express')
 const app = express()
 const AWS = require('aws-sdk');
 var s3 = new AWS.S3({signatureVersion: 'v4', region:"us-east-2"});
-
+const sqsClient = new AWS.SQS({region: 'us-east-1'});
 const MUSIC_TABLE = process.env.MUSIC_TABLE;
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 app.use(bodyParser.json({ strict: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -129,34 +130,37 @@ app.get('/song/:song', function (req, res) {
      if (err) console.log(err);
      else {
       console.log(data);
-      // var params = {
-      //   "Bucket": 'hw-bucket-name',
-      //   "Key": data.Items[0].info.song, 
-
-      // };
       var params = {Bucket: 'hw-bucket-name', Key: data.Items[0].info.song};
       var url = s3.getSignedUrl('getObject', params);
       console.log('The URL is', url);
       res.send({
         "song": url
       });
-      // var promise = s3.getSignedUrlPromise('getObject', params);
-      // promise.then(function(url) {
-      //   console.log('The URL is', url);
-      //   let response = {
-      //     "statusCode": 200,
-      //     "headers": {
-      //         "my_header": "my_value",
-      //         "Access-Control-Allow-Origin": "*",
-      //         "Access-Control-Allow-Credentials": true,
-      //     },
-      //     //"body": JSON.stringify(data.Contents,["Key"]),
-      //     "body": JSON.stringify(url),
-      //     "isBase64Encoded": false
-      //   };
-      //    res.status(null).send(response);
-      // }, function(err) { res.status(err).send(null); });
     }
   });
 })
+
+//Create play endpoint
+app.post('/play', function (req, res) {
+  const params = {
+    // TableName: MUSIC_TABLE,
+    MessageBody: 
+    JSON.stringify({
+      artist: req.body.artist,
+      album: req.body.album,
+      song: req.body.song
+    }),
+    QueueUrl: "https://sqs.us-east-1.amazonaws.com/829928713858/MyQueue"
+  };
+  sqsClient.sendMessage(params, function(err, data) {
+    if (err) {
+      console.log("Failed", err); // an error occurred
+    } else {
+      res.send({
+        "Success": true
+      });
+    }           // successful response
+  });
+})
+
 module.exports.handler = serverless(app);
